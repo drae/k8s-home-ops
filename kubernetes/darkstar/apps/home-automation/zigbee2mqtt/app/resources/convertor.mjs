236643545ad628dc1ab1c5952699638a45367171
Paul Owen <drae@users.noreply.github.com>
2025-10-14 20:45:30 +0100

    refactor(zigbee2mqtt): streamline convertor configuration in helm release and update resource file
    Signed-off-by: Paul Owen <drae@users.noreply.github.com>

--- kubernetes/darkstar/apps/home-automation/zigbee2mqtt/app/resources/convertor.mjs
+++ kubernetes/darkstar/apps/home-automation/zigbee2mqtt/app/resources/convertor.mjs
@@ -3,12 +3,12 @@ import * as m from 'zigbee-herdsman-converters/lib/modernExtend';
 export default {
     zigbeeModel: ['EGLO_ZM_TW'],
     model: '33955',
+    vendor: 'AwoX',
+    description: 'FUEVA-Z Ceiling Light',
     whiteLabel: [
         {vendor: "EGLO", model: "900316"},
         {vendor: "EGLO", model: "900317"},
         {vendor: "EGLO", model: "900053"},
     ],
-    vendor: 'AwoX',
-    description: 'FUEVA-Z Ceiling Light',
     extend: [m.light({colorTemp: {range: [153, 370]}, turnsOffAtBrightness1: true})],
 };
